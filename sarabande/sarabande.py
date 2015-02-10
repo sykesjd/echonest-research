e14c742b1919f8c1cf10c0bb670fdbb6585b5adc
sykesjd <jesse@dogwoodmicro.com>
2015-02-10 09:28:20 -0500

    added rubato for second of every three beats

--- sarabande/sarabande.py
+++ sarabande/sarabande.py
@@ -3,30 +3,38 @@
 """
 sarabande.py
 
-Digest only the second beat of every bar.
+Digest only the second beat of every bar and place rubato on second of every three beats.
 
-By Jesse David Sykes, 2015-02-06, based on one.py by Ben Lacker, 2009-02-18.
+By Jesse David Sykes, 2015-02-10
+Based on one.py by Ben Lacker, 2009-02-18, with additional code from simple_stretch.py by Thor Kell, 2013-11-18.
 """
 import echonest.remix.audio as audio
+import dirac
 
 usage = """
 Usage: 
     python sarabande.py <input_filename> <output_filename>
 
 Example:
-    python sarabande.py EverythingIsOnTheOne.mp3 EverythingIsReallyOnTheOne.mp3
+    python sarabande.py TwoForOne.mp3 ReallyTwoForOne.mp3
 """
 
 def main(input_filename, output_filename):
     audiofile = audio.LocalAudioFile(input_filename)
     bars = audiofile.analysis.bars
     collect = audio.AudioQuantumList()
+    count = 0
     for bar in bars:
-	try:
-            collect.append(bar.children()[1])
-	except IndexError:
-	    pass
-    out = audio.getpieces(audiofile, collect)
+		try:
+			beat = bar.children()[1]
+			beat_audio = beat.render()
+			scaled_beat = dirac.timeScale(beat_audio.data, 1.2) if count == 1 else dirac.timeScale(beat_audio.data, 1.0)
+			ts = audio.AudioData(ndarray=scaled_beat, shape=scaled_beat.shape, sampleRate=audiofile.sampleRate, numChannels=scaled_beat.shape[1])
+			collect.append(ts)
+			count = (count + 1) % 3
+		except IndexError:
+		    pass
+    out = audio.assemble(collect, numChannels=2)
     out.encode(output_filename)
 
 if __name__ == '__main__':
